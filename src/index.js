6aba17bf06fe5eb16d40ce9aee3b7dd248669ade
John <cooks.johns@gmail.com>
2018-04-23 15:47:07 -0500

    DOM PLAY

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 'use strict';
 
+
+const $ = require('jquery');
+
+
 let sayHello = greeting => console.log(greeting);
     sayHello('Hello');
 
+
+$('body').append(
+    '<h1> Click here</h1><br><div>Hit it</div>');
+
+$('h1').css(
+    'text-allign', 'center'
+
+);
+
+$(document).keyup(function(event){
+    alert(event.keyCode);
+});
+
+$('body').css('background-color', 'tan');
+
+
+$(document).ready(function(){
+    $("div").click(function(){
+        $("h1").css("color", "red");
+    });
+});
 
 
